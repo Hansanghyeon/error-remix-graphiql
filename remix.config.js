57fafbd44495ddbc02a5b7cf8999cfab61270e84
999 <hyeon@imweb.me>
2022-04-21 08:58:08 +0900

    Error Reproduction

--- remix.config.js
+++ remix.config.js
@@ -4,4 +4,8 @@
 module.exports = {
   cacheDirectory: "./node_modules/.cache/remix",
   ignoredRouteFiles: [".*", "**/*.css", "**/*.test.{js,jsx,ts,tsx}"],
+  serverDependenciesToBundle: [
+    'graphiql',
+    '@graphiql/toolkit',
+  ]
 };
